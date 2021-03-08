7841c8c6bd41e669b49c64a973bb10408ed7b1b4
nguyenhieunghia2001 <67048599+nguyenhieunghia2001@users.noreply.github.com>
2021-03-08 09:35:38 +0700

    scss install

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const handlebars  = require('express-handlebars');
 const app = express()
 const port = 3000
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'resource/views'));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('home')
 })
 
 app.listen(port, () => {
