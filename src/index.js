df58879d7862e11e1fba3e53cee5964db742d35f
nguyenhieunghia2001 <67048599+nguyenhieunghia2001@users.noreply.github.com>
2021-03-08 10:57:48 +0700

    test

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const handlebars  = require('express-handlebars');
 const app = express()
-const port = 3000
+const port = 8080
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.engine('handlebars', handlebars());
 app.set('view engine', 'handlebars');
